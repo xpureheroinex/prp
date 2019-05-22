9a7a3377f1f43b5e33dc356253d5bb2b495826a3
olhaklochko <olia.klochko1008@gmail.com>
2019-05-22 14:20:40 +0300

    edit top/recommend window

--- android/app/src/main/java/com/example/bookspace/UserActivity.java
+++ android/app/src/main/java/com/example/bookspace/UserActivity.java
@@ -27,13 +27,7 @@ private TextView mReadTextView;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
