aee7f799274d61c7116a1032b9a1ff73bb451c8b
olhaklochko <olia.klochko1008@gmail.com>
2019-05-26 14:37:00 +0300

    Statistic

---
+++ android/app/src/main/java/com/example/bookspace/StatisticActivity.java
@@ -0,0 +1,43 @@
+package com.example.bookspace;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class StatisticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistic_activity);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        StatisticYear startFragment = new StatisticYear();
+        transaction.add(R.id.ll2,startFragment);
+        transaction.commit();
+
+
+    }
+
+    public void onSelectFragment(View view) {
+        Fragment newFragment;
+        if (view == findViewById(R.id.btnfr1)) {
+            newFragment = new StatisticYear();
+        } else if (view == findViewById(R.id.btnfr2)) {
+            newFragment = new StatisticMonth();
+        } else if (view == findViewById(R.id.btnfr3)) {
+            newFragment = new StatisticWeek();
+        } else {
+            newFragment = new StatisticYear();
+        }
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.ll2,newFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+}
