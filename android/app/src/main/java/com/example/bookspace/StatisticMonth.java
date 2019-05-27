4de5cf4a09ee52647aad2df5a3ce6ed3def0078b
olhaklochko <olia.klochko1008@gmail.com>
2019-05-27 19:59:04 +0300

    margin in statistics

--- android/app/src/main/java/com/example/bookspace/StatisticMonth.java
+++ android/app/src/main/java/com/example/bookspace/StatisticMonth.java
@@ -12,6 +12,6 @@ public class StatisticMonth extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.statisticMonth,container,false);
+        return inflater.inflate(R.layout.statistic_month,container,false);
     }
 }
