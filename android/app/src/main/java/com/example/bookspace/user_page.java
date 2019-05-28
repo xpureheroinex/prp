edd65aa86ab145eb401368e83653ed65d58a15f6
Alexander Skoruk <oleksandr.skoruk@nure.ua>
2019-05-28 14:08:01 +0300

    bug fixes
    Signed-off-by: Alexander Skoruk <oleksandr.skoruk@nure.ua>

--- android/app/src/main/java/com/example/bookspace/user_page.java
+++ android/app/src/main/java/com/example/bookspace/user_page.java
@@ -66,10 +66,7 @@ public class user_page extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
