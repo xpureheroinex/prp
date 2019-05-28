5e1ec8df323e83833fcfa18e13184e515b9131f0
xXx-ZhenYa-xXx <zhenya_yarushnikov@inbox.ru>
2019-05-28 17:23:44 +0400

    Add reviews & notes

---
+++ android/app/src/main/java/com/example/bookspace/NoticeActivity.java
@@ -0,0 +1,32 @@
+package com.example.bookspace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+
+        TextView textAbout = findViewById(R.id.textAbout);
+        textAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BookPageActivity.class));
+            }
+        });
+
+        TextView textReviews = findViewById(R.id.textReviews);
+        textReviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NoticeActivity.class));
+            }
+        });
+    }
+}
---
+++ android/app/src/main/java/com/example/bookspace/ReviewsActivity.java
@@ -0,0 +1,32 @@
+package com.example.bookspace;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+
+        TextView textAbout = findViewById(R.id.textAbout);
+        textAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BookPageActivity.class));
+            }
+        });
+
+        TextView textNotice = findViewById(R.id.textNotice);
+        textNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NoticeActivity.class));
+            }
+        });
+    }
+}
