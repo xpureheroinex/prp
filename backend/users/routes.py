ace76acdc7653af0e4e4fd0986ef5c3415220027
Gurova-J <vgurovav@gmail.com>
2019-06-03 23:25:22 +0300

    Finish search

--- backend/users/routes.py
+++ backend/users/routes.py
@@ -1,3 +1,6 @@
+import re
+
+from sqlalchemy import or_
 from werkzeug.security import generate_password_hash
 from werkzeug.utils import redirect
 
@@ -5,7 +8,7 @@ from backend import db, api
 from flask_restful import Resource, reqparse
 from flask import request, jsonify
 
-from backend.models import Books
+from backend.models import Books, User
 from . import bp
 
 _BAD_REQUEST = {'status': 'error'}, 400, 500
@@ -16,36 +19,37 @@ class Search(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('search')
+        self.parser.add_argument('Authorization', location='headers')
 
     def post(self):
         args = self.parser.parse_args()
         search = args['search']
-        info = []
-        list_books_genre = Books.query.filter_by(genre=search).all()
-        list_books_author = Books.query.filter_by(author=search).all()
-        list_books_title = Books.query.filter_by(title=search).all()
-        for book1 in list_books_genre:
-            genre = {
-                "Title": book1.title,
-                "Author": book1.author,
-                "Genre": book1.genre
-            }
-            info.append(genre)
-        for book2 in list_books_author:
-            author = {
-                "Title": book2.title,
-                "Author": book2.author,
-                "Genre": book2.genre
-            }
-            info.append(author)
-        for book3 in list_books_title:
-            title = {
-                "Title": book3.title,
-                "Author": book3.author,
-                "Genre": book3.genre
-            }
-            info.append(title)
-        return {'len': len(list_books_genre), 'genre': info}
+        if args['Authorization'] is None:
+            return {'message': 'Unauthorized', 'status': 401}
+        token = args['Authorization'].split(' ')[1]
+        if User.verify_auth_token(token) is None:
+            return {'message': 'Unauthorized', 'status': 401}
+        user_id = User.verify_auth_token(token)['user_id']
+        user = User.query.get(user_id)
+        if user is None:
+            return _BAD_REQUEST
+        elif search is not None:
+            info = []
+            research = search.strip()
+            s = f'%{research}%'
+            print(research)
+            result = Books.query.filter(or_(Books.genre.ilike(s), Books.title.ilike(s), Books.author.ilike(s))).all()
+            print(result)
+
+            for book in result:
+                listbook = {
+                    "id": book.id,
+                    "Title": book.title,
+                    "Author": book.author,
+                    "Genre": book.genre
+                }
+                info.append(listbook)
+        return {'count': len(info), 'Results of searching': info, 'status': 200}
 
 
 api.add_resource(Search, '/books/search')
