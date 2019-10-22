bcad17f0d25ff35e6f48194f8bdf7cb00fd3f659
xXx-ZhenYa-xXx <zhenya_yarushnikov@inbox.ru>
2019-10-22 18:25:53 +0300

    add necessary files

---
+++ android/app/src/main/java/com/example/bookspace/model/profile/ImageResponse.java
@@ -0,0 +1,41 @@
+package com.example.bookspace.model.profile;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ImageResponse {
+
+    @SerializedName("status")
+    @Expose
+    private int status;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
