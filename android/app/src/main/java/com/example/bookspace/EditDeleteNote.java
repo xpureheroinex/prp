dae9820d6a6485a718c0f7c18bcaf6c2e06ffd56
Alexander Skoruk <oleksandr.skoruk@nure.ua>
2019-11-27 01:01:44 +0200

    small fixes
    Signed-off-by: Alexander Skoruk <oleksandr.skoruk@nure.ua>

--- android/app/src/main/java/com/example/bookspace/EditDeleteNote.java
+++ android/app/src/main/java/com/example/bookspace/EditDeleteNote.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -46,12 +47,28 @@ public class EditDeleteNote extends AppCompatActivity {
 
         EditText eTitle = findViewById(R.id.editTitleEdit);
         eTitle.setText(Title, TextView.BufferType.EDITABLE);
-
         EditText eNote = findViewById(R.id.editAddNoteEdit);
         eNote.setText(Text, TextView.BufferType.EDITABLE);
 
         SharedPreferences prefs = getSharedPreferences("AppPreferences", MODE_PRIVATE);
         token = prefs.getString("token", "token is null");
+
+        //region настраиваем toolbar
+        Toolbar basicToolbar = findViewById(R.id.basicToolbar);
+        basicToolbar.setTitle(R.string.editDeleteNoteTitle);
+        basicToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        setSupportActionBar(basicToolbar);
+//        getSupportActionBar().setTitle(R.string.editDeleteNoteTitle);
+        //endregion
+
+        //region устанавливаем обработчик нажатия для back arrow иконки
+        basicToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        //endregion
 
     }
 
